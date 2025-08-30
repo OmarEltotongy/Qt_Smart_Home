651e1bb124d713a48d23914ea15f4eb8378c2ab7
OmarEltotongy <omareltotongy@gmail.com>
2025-08-30 16:06:02 +0300

    V 1.0 - Designed UI - Separeted the design from the style using Qss file

---
+++ mainwindow.cpp
@@ -0,0 +1,32 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QFile>
+#include <QApplication>
+#include <QResource>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    // Load and apply the stylesheet from Qt resources
+    QFile styleFile(":/stylesheet.qss"); // Note the :/ prefix for resources
+    if (styleFile.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        QString styleSheet = QLatin1String(styleFile.readAll());
+        qApp->setStyleSheet(styleSheet); // Apply to entire application
+        styleFile.close();
+    }
+    else
+    {
+        // Optional: Print error if stylesheet couldn't be loaded
+        qDebug() << "Could not open stylesheet file:" << styleFile.errorString();
+    }
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
